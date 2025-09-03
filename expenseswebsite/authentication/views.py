9a3aa49a2b707e7ab46536c7b8e9b0cc32084313
Rachel Davey <rachel.davey@libfstudy.ac.uk>
2025-09-03 17:21:15 +0200

    Implement username validation logic and import User model

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 import json
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -10,7 +11,10 @@ class UsernameValidationView(View):
         data = json.loads(request.body)
         username = data['username']
         if not str(username).isalnum():
-            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'})
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Username is already taken'},status=409)
+        return JsonResponse({'username_valid': True}, status=200)
 
 class RegistrationView(View):
     def get(self, request):
