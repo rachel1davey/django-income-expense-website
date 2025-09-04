58c15770417aa6a45572701993edf30b47e5966f
Rachel Davey <rachel.davey@libfstudy.ac.uk>
2025-09-04 18:32:32 +0200

    Add success message functionality to registration view

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 
 # Create your views here.
 
@@ -30,3 +31,7 @@ class UsernameValidationView(View):
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+    def post(self, request):
+        messages.success(request, 'Account successfully created')
+        return render(request, 'authentication/register.html')
+
